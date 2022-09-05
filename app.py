86ed8ab139fe3e065f5ac12156755286ca6c95d8
Kishore Kumar <kishore1234500000@gmail.com>
2022-09-05 19:14:36 +0530

    create medal_tally function and load medal_tally table

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-st.sidebar.radio(
+import helper
+import preprocessor
+
+df = pd.read_csv("./dataset/athlete_events.csv")
+region_df = pd.read_csv("./dataset/noc_regions.csv")
+
+df = preprocessor.preprocess(df, region_df)
+
+user_menu = st.sidebar.radio(
     "Select an option",
     ("Medal Tally", "Overall analysis", "Country wise analysis", "Athlete wise analysis")
 )
 
+if user_menu == "Medal Tally":
+    medal_tally = helper.medal_tally(df)
+    st.table(medal_tally)
--- helper.py
+++ helper.py
@@ -1,8 +1,8 @@
 def medal_tally(mtdf):
-    tally = mtdf.drop_duplicates(subset=["Team", "Noc", "Games", "Year", "City", "Sport", "Event", "Medal"]).groupby("region").sum()[["Gold", "Silver", "Bronze"]].sort_values("Gold", ascending=False).reset_index()
+    tally = mtdf.drop_duplicates(subset=["Team", "NOC", "Games", "Year", "City", "Sport", "Event", "Medal"]).groupby("region").sum()[["Gold", "Silver", "Bronze"]].sort_values("Gold", ascending=False).reset_index()
     tally["Total"] = tally["Gold"] + tally["Silver"] + tally["Bronze"]
     tally["Gold"] = tally["Gold"].astype("int")
     tally["Silver"] = tally["Silver"].astype("int")
     tally["Bronze"] = tally["Bronze"].astype("int")
+    tally["Total"] = tally["Total"].astype("int")
     return tally
-

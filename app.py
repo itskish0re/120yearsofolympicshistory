de6b0a4a9b99ec1a53cb6a87638073d385f6e0e1
Kishore Kumar <kishore1234500000@gmail.com>
2022-10-02 10:13:26 +0530

    Create Overall analysis and add some top stats

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 import helper
 import preprocessor
+import plotly.express as px
 
 df = pd.read_csv("./dataset/athlete_events.csv")
 region_df = pd.read_csv("./dataset/noc_regions.csv")
@@ -12,7 +13,7 @@ st.sidebar.title("120 Years Of Olympics Data")
 
 user_menu = st.sidebar.radio(
     "Select an option",
-    ("Medal Tally", "Overall analysis", "Country wise analysis", "Athlete wise analysis")
+    ("Medal Tally", "Overall Analysis", "Country Wise Analysis", "Athlete Wise Analysis")
 )
 
 if user_menu == "Medal Tally":
@@ -30,3 +31,34 @@ if user_menu == "Medal Tally":
     elif selected_year != "Overall" and selected_country != "Overall":
         st.title(selected_country + " performance in " + str(selected_year) + " Olympics")
     st.table(medal_tally)
+
+if user_menu == "Overall Analysis":
+    edition = df["Year"].unique().shape[0]-1
+    cities = df["City"].unique().shape[0]
+    sports = df["Sport"].unique().shape[0]
+    events = df["Event"].unique().shape[0]
+    athletes = df["Name"].unique().shape[0]
+    nations = df["region"].unique().shape[0]
+
+    st.title("Top Statstics")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Editions")
+        st.title(edition)
+    with col2:
+        st.header("Hosts")
+        st.title(cities)
+    with col3:
+        st.header("Sports")
+        st.title(sports)
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Events")
+        st.title(events)
+    with col2:
+        st.header("Athletes")
+        st.title(athletes)
+    with col3:
+        st.header("Nations")
+        st.title(nations)
